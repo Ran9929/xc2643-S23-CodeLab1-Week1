fea4ccf086344b5f7cf9fe5a28a2e16bffa4bcf4
Ran99 <cxran999@gmail.com>
2023-01-23 13:20:11 -0500

    Add new Unity Project

---
+++ CodeLab1_Week1/Assets/Scripts/WASDController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDController : MonoBehaviour
+{
+    public float forceAmount = 0;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W Pressed");
+            rb.AddForce(0, 0, forceAmount);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("S Pressed");
+            rb.AddForce(0, 0, -forceAmount);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("A Pressed");
+            rb.AddForce(-forceAmount, 0, 0 );
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D Pressed");
+            rb.AddForce(forceAmount, 0, 0 );
+        }
+    }
+}
